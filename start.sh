4ae88a4636d01aef8e6f8b3118c1d29d7f1f042c
wuyu800322 <wuyu800322@gmail.com>
2024-12-30 10:57:51 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+#Before running the code, please ensure that the following compressed files are extracted:
+
+#PassGAN/hashpwd/hashed_gen_passwords.txt.zip
+#PassGAN/data/train.txt.zip
+#After extraction, you should have:
+
+unzip PassGAN/hashpwd/hashed_gen_passwords.txt.zip -d PassGAN/hashpwd/
+unzip PassGAN/data/train.txt.zip -d PassGAN/data/
+
 cd /Users/xiakejie/PassMon/PassMon/PassGAN
 
 source ./passganenv/bin/activate
